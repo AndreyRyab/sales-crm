5f8eeae5ce1b1f391474e83834249d66154791e5
Andrey Ryabtsev <ryab@yandex.ru>
2022-01-05 15:50:09 +0300

    side-bar and floating btn (not active)

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,7 +1,13 @@
+import { ElementRef } from "@angular/core";
+
 declare var M: any; // in order to have no TS problems
 
 export class MaterialService {
   static toast(message: string) {
     M.toast({ html: message });
+  }
+
+  static initializeFloatingButton(ref: ElementRef) {
+    M.FloatingActionButton.init(ref.nativeElement);
   }
 }
--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -40,7 +40,7 @@ export class AuthService {
     return !!this.token;
   }
 
-  logout(): void {
+  logout() {
     this.setToken(null);
     localStorage.clear();
   };
