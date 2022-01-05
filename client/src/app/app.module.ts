18ffb880f33d8fa82c7b3b686f0488f62775234f
Andrey Ryabtsev <ryab@yandex.ru>
2022-01-05 21:14:38 +0300

    pages added + refactoring with async pipe rxjs

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { AnalyticsPageComponent } from './analytics-page/analytics-page.componen
 import { HistoryPageComponent } from './history-page/history-page.component';
 import { OrderPageComponent } from './order-page/order-page.component';
 import { CategoriesPageComponent } from './categories-page/categories-page.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { CategoriesFormComponent } from './categories-page/categories-form/categories-form.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import { CategoriesPageComponent } from './categories-page/categories-page.compo
     AnalyticsPageComponent,
     HistoryPageComponent,
     OrderPageComponent,
-    CategoriesPageComponent
+    CategoriesPageComponent,
+    LoaderComponent,
+    CategoriesFormComponent
   ],
   imports: [
     BrowserModule,
