57acb442aeaa6f64a1631fbf96a6459b6026e1a3
Andrey Ryabtsev <ryab@yandex.ru>
2022-01-06 15:59:38 +0300

    add category page

--- client/src/app/categories-page/categories-form/categories-form.component.ts
+++ client/src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params } from '@angular/router';
+import { of, switchMap } from 'rxjs';
+import { MaterialService } from 'src/app/shared/classes/material.service';
+import { CategoriesService } from 'src/app/shared/services/categories.service';
 
 @Component({
   selector: 'app-categories-form',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories-form.component.css']
 })
 export class CategoriesFormComponent implements OnInit {
+  form!: FormGroup;
+  isNew = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private categoriesService: CategoriesService) { };
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required)
+    })
+
+    /* this.route.params.subscribe((params: Params) => {
+      if (params['id']) {
+        // Editing the form
+        this.isNew = false;
+      }
+    }); */
+    this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            if (params['id']) {
+              this.isNew = false;
+              return this.categoriesService.getById(params['id']);
+            }
+
+            return of(null);
+          }
+        )
+      )
+      .subscribe({
+        next: category => {
+          if (category) {
+            this.form.patchValue({
+              name: category.name,
+            });
+            MaterialService.updateTextInputs();
+          }
+        },
+        error: error => MaterialService.toast(error.error.message)
+      })
+
+  };
+
+  onSubmit() {
+
+  };
 
 }
