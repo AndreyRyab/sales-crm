d7bda04ebcbaec8c66df7bdea35e74565163592a
Andrey Ryabtsev <ryab@yandex.ru>
2022-01-12 15:45:27 +0300

    history components and tooltip

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { MaterialInstance, MaterialService } from '../shared/classes/material.service';
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('tooltip') tooltipRef!: ElementRef
+
+  isFilterVisible = false;
+  tooltip!: MaterialInstance;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+
+  };
+
+  ngOnDestroy() {
+    this.tooltip.destroy();
+  };
+
+  ngAfterViewInit() {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef);
+  };
 
 }
